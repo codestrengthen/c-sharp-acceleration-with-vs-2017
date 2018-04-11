217b2531134822f8dee6d63223f3ae3bd561102d
codestrengthen <33879202+codestrengthen@users.noreply.github.com>
2018-04-11 19:38:10 +1000

    Module: Field and Property

---
+++ Module E - Field and Property/MovieConsoleApp/MovieConsoleApp/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieConsoleApp
+{
+    class Movie
+    {
+        public string MovieName { get; set; }
+
+        public int MyProperty { get; set; }
+
+        private DateTime releaseDate;
+        public DateTime ReleaseDate
+        {
+            get { return releaseDate; }
+
+            set { releaseDate = value; }
+        }
+
+        private double movieRating;
+        public double MovieRating
+        {
+            get { return movieRating; }
+
+            set { movieRating = value; }
+        }
+
+        public void TestMethod()
+        {
+            this.MovieName = "Frozen";
+        }
+    }
+}
