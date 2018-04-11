093b0a8bf90f4405d6880702898fc90a887a9aee
codestrengthen <33879202+codestrengthen@users.noreply.github.com>
2018-04-11 17:02:40 +1000

    Module: Visual Studio Project

---
+++ Module A - Visual Studio Project/MyFirstApplication/MyFirstApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Great things start with the first step!");
+            
+        }
+    }
+}
