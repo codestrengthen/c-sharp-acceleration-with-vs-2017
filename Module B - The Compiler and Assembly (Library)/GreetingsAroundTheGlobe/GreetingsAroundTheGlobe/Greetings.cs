71953c38ed25448f2fcf5d822b7b584f768af41f
codestrengthen <33879202+codestrengthen@users.noreply.github.com>
2018-04-11 17:11:21 +1000

    Module B - The Compiler and Assembly (Library)

---
+++ Module B - The Compiler and Assembly (Library)/GreetingsAroundTheGlobe/GreetingsAroundTheGlobe/Greetings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingsAroundTheGlobe
+{
+    public class Greetings
+    {
+        internal static Dictionary<string, string> HelloLookup { get; private set; }
+        
+        public static string SayHelloInLanguage(string country)
+        {
+            //Instantiate the dictionary lookup of country and language
+            HelloLookup = new Dictionary<string, string>()
+            {
+                { "america", "Hello!" },
+                { "australia", "Good day mate!" },
+                { "england", "Hello!" },
+                { "france", "Bonjour!" },
+                { "spain", "Hola!" },
+                { "italy", "Ciao!" },
+                { "india", "Namaste" },
+                { "japan", "Ohayo!" },
+                { "korea", "Ahn-Young-Ha-Se-Yo!" },
+                { "indonesia", "Halo!"},
+                { "china", "Ni Hau!"},
+                { "vietnam", "Xin chao!"}
+            };
+
+            string lowercaseCountry = country.ToLower();
+            if (HelloLookup.TryGetValue(lowercaseCountry, out string greeting))
+            {
+                return greeting;
+            }
+            return "We're not sure how to say hi in your language... Let's just say Hello! :)";
+        }
+    }
+}
---
+++ Module B - The Compiler and Assembly (Library)/MyFirstApplication/MyFirstApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreetingsAroundTheGlobe;
+
+namespace MyFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Where do you come from?");
+                string country = Console.ReadLine();
+                if (String.IsNullOrEmpty(country))
+                {
+                    Console.WriteLine("Bye for now!");
+                    break;
+                }
+                //call the method in the assembly
+                Console.WriteLine(Greetings.SayHelloInLanguage(country));
+            }
+        }
+    }
+}
